77b79c72a45d647f4bf3a2690604fb3bc03301ec
TakHi <TakHi@users.noreply.github.com>
2015-09-07 00:20:27 +0900

    Added adept check at Homeport

--- src/CvwSkillNotifier.cs
+++ src/CvwSkillNotifier.cs
@@ -1,19 +1,83 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using CvwSkillNotifier.Model;
 using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleWrapper.Models.Raw;
 
 namespace CvwSkillNotifier
 {
     [Export(typeof(IPlugin))]
+    [Export(typeof(IRequestNotify))]
+    [Export(typeof(INotifier))]
     [ExportMetadata("Guid", "0FDACB8C-8AD7-400D-9D0C-037CD86A330E")]
     [ExportMetadata("Title", "CVWSkillNotifier")]
-    [ExportMetadata("Description", "空母航空隊の熟練度が最大になった際に通知を行います。")]
-    [ExportMetadata("Version", "1.0")]
+    [ExportMetadata("Description", "艦上機/艦載機の熟練度が最大になった際に通知を行います。")]
+    [ExportMetadata("Version", "0.1.20150907")]
     [ExportMetadata("Author", "@hgzr")]
-    public class CvwSkillNotifier : IPlugin
+    public class CvwSkillNotifier : IPlugin, IRequestNotify, INotifier
     {
+        private List<CvWing> _cvWings;
+        
+        public event EventHandler<NotifyEventArgs> NotifyRequested;
+
         public void Initialize()
         {
-            throw new System.NotImplementedException();
+            KanColleClient.Current.Proxy.api_port.TryParse<kcsapi_port>().Subscribe(x => PortSkillCheck());
+            _cvWings = new List<CvWing>();
+        }
+
+        public void PortSkillCheck()
+        {
+            var slotItemData = KanColleClient.Current.Homeport.Itemyard.SlotItems.Values.Where((si) => si.Info.IsNumerable);
+            SkillNotifyCheck(slotItemData);
+        }
+        
+        public void SkillNotifyCheck(IEnumerable<SlotItem> slotItemData )
+        {
+            var notifyIds = new List<int>();
+
+            foreach (var currentCvWing in slotItemData.Select(slotItem => new CvWing {Id = slotItem.Id, Name = slotItem.Info.Name,
+                SkillLevel = slotItem.Adept, PreviousSkillLevel=slotItem.Adept, RowData = slotItem}))
+            {
+                var cvWingIndex = _cvWings.FindIndex((cvw) => cvw.Id == currentCvWing.Id);
+                if (cvWingIndex == -1)
+                {
+                    _cvWings.Add(currentCvWing);
+                }
+                else
+                {
+                    _cvWings[cvWingIndex].SkillLevel = currentCvWing.SkillLevel;
+                    if (_cvWings[cvWingIndex].PreviousSkillLevel != 7 && _cvWings[cvWingIndex].SkillLevel == 7)
+                    {
+                        notifyIds.Add(cvWingIndex);
+                    }
+                    _cvWings[cvWingIndex].PreviousSkillLevel = _cvWings[cvWingIndex].SkillLevel;
+                }
+            }
+
+            if (notifyIds.Any())
+            {
+                string notifyBody;
+                if (notifyIds.Count == 1)
+                {
+                    notifyBody = _cvWings[notifyIds[0]].Name + "の熟練度が最大になりました。";
+                }
+                else
+                {
+                    notifyBody = notifyIds.Count.ToString() + "機の艦載機の熟練度が最大になりました。";
+                }
+
+                NotifyRequested?.Invoke(this, new NotifyEventArgs("CVWSkillNotify", "艦載機熟練度通知", notifyBody));
+            }
+        }
+
+        public void Notify(INotification notification)
+        {
+            Notification.Create(notification.Type, notification.Header, notification.Body);
         }
     }
 }
---
+++ src/Model/CvWing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper.Models;
+
+namespace CvwSkillNotifier.Model
+{
+    class CvWing
+    {
+        public int Id { get; set; } = -1;
+        public string Name { get; set; } = "九七式艦攻+七一式電探（蓮見隊）";
+        public int SkillLevel { get; set; } = -1;
+        public int PreviousSkillLevel { get; set; } = -1;
+        public SlotItem RowData { get; set; } = null;
+    }
+}
